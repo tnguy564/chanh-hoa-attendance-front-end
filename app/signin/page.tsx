32a107c15c08eb7af09a88c52391c4bcba4b9201
tnguy564 <116107071+tnguy564@users.noreply.github.com>
2026-03-08 15:52:28 -0700

    Update page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -37,7 +37,7 @@ export default function SignInPage() {
     setStatus("Signing in...");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/signin", {
+      const res = await fetch("${process.env.NEXT_PUBLIC_API_URL}/api/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
