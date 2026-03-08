8c1d276ff414c44953c83117df909fca734e9d70
tnguy564 <116107071+tnguy564@users.noreply.github.com>
2026-03-08 15:53:45 -0700

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -35,7 +35,7 @@ export default function SignUpPage() {
     setStatus("Registering...");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/signup", {
+      const res = await fetch("${process.env.NEXT_PUBLIC_API_URL}/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
